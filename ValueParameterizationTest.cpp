8311b7da9ef319d8d401128f0ef6770ff5f08402
Saipavan-Poosala <saipavanpoosala@gmail.com>
2024-09-18 14:34:20 +0530

    Update ValueParameterizationTest.cpp

--- ValueParameterizationTest.cpp
+++ ValueParameterizationTest.cpp
@@ -7,39 +7,7 @@ protected:
     string input;
     int expectedValue;
     int actualValue;
-};#include "StringCalculator.h"
-#include <gtest/gtest.h>
-#include <tuple>
-
-class StringCalculatorTestFixture : public ::testing::Test {
-protected:
-    string input;
-    int expectedValue;
-    int actualValue;
 };
-
-class StringCalculatorParameterFixture : public StringCalculatorTestFixture,
-                                        public testing::WithParamInterface<tuple<string, int>> {
-protected:
-    void SetUp() override {
-        // Set up parameters for the test based on the current test case
-        input = std::get<0>(GetParam());
-        expectedValue = std::get<1>(GetParam());
-    }
-};
-
-// Parameter Values for testing
-INSTANTIATE_TEST_SUITE_P(ValidStringCalculatorInputs, StringCalculatorParameterFixture,testing::Values(
-                             make_tuple("", 0),
-                             make_tuple("1", 1),
-                             make_tuple("1,2", 3),
-                             make_tuple("1,1000", 1001)
-                         ));
-
-TEST_P(StringCalculatorParameterFixture, ParameterizedTest) {
-    //actualValue = Add(input);
-    //ASSERT_EQ(actualValue, expectedValue);
-}
 class StringCalculatorParameterFixture:public StringCalculatorFixture, public testing::WithParamInterface<tuple<string,int>>{
 
 };
@@ -56,9 +24,9 @@ INSTANTIATE_TEST_SUITE_P(ValidStringCalculatorInputs,StringCalculatorParameterFi
 TEST_P(StringCalculatorParameterFixture,ParameterizedTest){
       input= std::get<0>(GetParam());
       expectedValue= std::get<1>(GetParam());
-      Add input();
-      //actualValue=Add(input);
-      //ASSERT_EQ(actualValue,expectedValue);
+    Add(input);
+     // actualValue=Add(input);
+     // ASSERT_EQ(actualValue,expectedValue);
 }
 
 
